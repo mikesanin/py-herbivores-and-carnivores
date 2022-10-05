db517b321f6d4bc0db8472c180e481c7dbbd5c88
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2022-10-05 14:03:07 +0300

    Update requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str) -> None:
+        self.health = 100
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def set_health(self, health: int) -> None:
+        self.health = health
+        if self.health <= 0:
+            self.die()
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+def bite(target: Animal) -> None:
+    if isinstance(target, Herbivore) and not target.hidden:
+        target.set_health(target.health - 50)
+
+
+class Carnivore(Animal):
+    pass
