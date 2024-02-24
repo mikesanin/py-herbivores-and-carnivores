4f9557e1358cd673c7d3965c8cb065c2f41f0128
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2024-02-24 22:20:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,11 +17,11 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, "
-                   Health: {self.health}, "
-                   Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
-
+                
 class Herbivore(Animal):
     def hide(self) -> None:
         self.hidden = not self.hidden
